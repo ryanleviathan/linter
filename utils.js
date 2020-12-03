e0b365c8a384a25f582ff96669d206b8bf7f8948
ryanleviathan <ryan.m.carreras@gmail.com>
2020-12-03 15:40:40 -0800

    stripInput splits each

--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
+const { array } = require("yargs")
+
 function stripInput(input) {
-  return input.replace(/[\-A-Za-z0-9,"'+*/=.,;:&$!?@#%^<>|_]/g, "") 
+  const newString = input.replace(/[\-A-Za-z0-9,"'+*/=.,;:&$!?@#%^<>|_]/g, "")
+  let array = newString.split('')
+  return array
 }
 
 const wonkyWord = stripInput('te(s)t{i}n[g]')
